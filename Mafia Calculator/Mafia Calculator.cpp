78e2cd0fa8577d00c15cc69f1c1180bbe0752114
kilri <kilri@DEVROCK>
2024-02-16 23:10:22 +0000

    Updated documention

--- Mafia Calculator/Mafia Calculator.cpp
+++ Mafia Calculator/Mafia Calculator.cpp
@@ -1,13 +1,11 @@
 //This is a calculator created for the social deduction game Mafia
 //It calculates the percentage chance of the Mafia players winning the game
-
 #include <iostream>
 #include <cmath>
 
 int main() {
     double players = 10.0;
     double mafia = 2.0;
-
     double percentage = mafia / (sqrt(players));
     std::cout << percentage << std::endl;
 }
